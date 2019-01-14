4e692baadffcc5764bad81e6dfa6aa64ecc1798b
lblahuto <lblahuto@redhat.com>
2019-01-14 12:57:07 +0100

    another setting for opennshift

---
+++ server.js
@@ -0,0 +1,41 @@
+// import express from 'express';
+// import webpack from 'webpack';
+// import path from 'path';
+// import config from '../webpack.config.dev';
+// import open from 'open';
+// import favicon from 'serve-favicon';
+//
+// /* eslint-disable no-console */
+//
+// const app = express();
+// const compiler = webpack(config);
+var express = require('express');
+var webpack = require('webpack');
+const path = require('path');
+// var config = require('../webpack.config.js');
+var app = express();
+// var compiler = webpack();
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+// app.use(require('webpack-dev-middleware')(compiler, {
+//   noInfo: true,
+//   publicPath: config.output.publicPath
+// }));
+
+// app.use(require('webpack-hot-middleware')(compiler));
+// app.use(favicon(path.join(__dirname,'assets','public','favicon.ico')));
+//
+// app.get('*', function(req, res) {
+//   res.sendFile(path.join( __dirname, '../src/index.html'));
+// });
+
+app.listen(server_port, server_ip_address, function(err) {
+  console.log( "Listening on " + server_ip_address + ", port " + server_port )
+});
