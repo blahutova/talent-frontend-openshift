6a39ff2a1140f1b1093c70c763db1b6bc09d3fa0
lblahuto <lblahuto@redhat.com>
2019-01-14 13:25:32 +0100

    fix server.js host

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const path = require('path');
 var app = express();
 // var compiler = webpack();
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
 
 app.use(express.static(path.join(__dirname, 'build')));
 
